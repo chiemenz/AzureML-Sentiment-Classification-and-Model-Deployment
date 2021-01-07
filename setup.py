9b181c1aeb8406b31cc32e1a3d8fbde920ba6326
christoph.hiemenz <christoph.hiemenz@web.de>
2021-01-07 16:08:48 +0100

    a setup.py has been added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chiemenz",
+    version="0.0.1",
+    author="Christoph Hiemenz",
+    author_email="christoph.hiemenz@web.de",
+    description="This is a Repo to demonstrate the use of azureml automl and azureml hyperdrive",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chiemenz/automl_vs_hyperdrive",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

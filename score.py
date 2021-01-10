2c16ef1ffb75178c3a6f3f0c3e36c5d97d9fbdae
christoph.hiemenz <christoph.hiemenz@web.de>
2021-01-10 16:15:01 +0100

    updated train script for more efficient training

--- score.py
+++ score.py
@@ -1,21 +1,27 @@
 import json
-import numpy as np
 import os
-from sklearn.externals import joblib
+
+import joblib
+import numpy as np
+import scipy.sparse
+import xgboost as xgb
 
 
 def init():
     global model
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It's the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION).
+    # For multiple models, it points to the folder containing all deployed models (./azureml-models).
     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'xgboost_model.pkl')
     model = joblib.load(model_path)
 
 
-def run(data):
-    try:
-        data = np.array(json.loads(data))
-        result = model.predict(data)
-        # You can return any data type, as long as it is JSON serializable.
-        return result.tolist()
-    except Exception as e:
-        error = str(e)
-        return error
+def run(raw_data):
+    data = np.array(json.loads(raw_data)['data'])
+    csr_data = scipy.sparse.csr_matrix(data)
+    test_data = xgb.DMatrix(csr_data)
+
+    # Make prediction.
+    y_hat = model.predict(test_data)
+    # You can return any data type as long as it's JSON-serializable.
+    return y_hat.tolist()
